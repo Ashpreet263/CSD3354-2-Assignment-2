5476d9031e8e2b5e098bcdf7c2129738d5398065
visualstudio <visualstudio@122-PC35>
2019-03-06 10:45:09 -0500

    CSD3354-2-lab 2

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSD3354_2_Assignment_2
+
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1(int i)
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method2);
+            myDelegate();
+        }
+
+    }
+}
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
